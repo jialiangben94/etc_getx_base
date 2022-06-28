1a828c336be48bff4b067bfcbf0e34ba9cdb8363
Benjamin <benjaminetc@etctech.com.my>
2022-06-28 15:08:15 +0800

    Committed changes

--- etc_getx_base/__brick__/{{project_route}}/lib/main.dart
+++ etc_getx_base/__brick__/{{project_route}}/lib/main.dart
@@ -1,67 +1,38 @@
+import 'package:{{project_route}}/app/routes/app_routes.dart';
+import 'package:{{project_route}}/app/ui/global/global_binding.dart';
+import 'package:{{project_route}}/app/ui/loading/loading_page.dart';
+import 'package:{{project_route}}/app/utils/app_theme.dart';
+import 'package:{{project_route}}/app/utils/pro_base_state.dart';
+import 'package:{{project_route}}/app/utils/share_pref.dart';
+import 'package:{{project_route}}/app/utils/translations/app_translations.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ProBaseState.allowedOrientation([DeviceOrientation.portraitUp]);
+  await SharePref.sharePref.initialize();
+  ProBaseState.onFailed = (context, code, message) {
+    if (code == 401 || code == 426) {
+      return true;
+    }
+    return false;
+  };
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: '{{{project_name}}}',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      theme: appThemeData,
+      getPages: AppPages.routes,
+      initialRoute: LoadingPage().routeName,
+      initialBinding: GlobalBinding(),
     );
   }
 }
